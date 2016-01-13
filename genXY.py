4bbcebbb23d0fdde6d4ae540327a62d5093e9117
NickRRomero <nromo01@gmail.com>
2016-01-13 15:38:05 -0800

    Python script used to generate input files
    Files generate have following format, XY_#.cell where # is a squared
    value for the total number of cells. The first line in the file is the
    number of fixed value cells. The rest of the file contains x y pairs
    followed by a fixed cell value

---
+++ genXY.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import random
+import math
+import os
+import sys
+
+def getCellNumber():
+   return (raw_input("Please enter a number of cells, i.e. 1, 4, 9, 16, 25\n"),
+           raw_input("Please enter a lower limit on the fixed cell values\n"),
+           raw_input("Please Enter an upper limit on the fixed cell values\n"))
+   
+def openFile(cellNumber):
+   newFile = 'XY_' + str(cellNumber) + '.cell'
+   return open(newFile, 'w')
+
+def writeToFile(newFile, buildFile, g):
+   for key, value in g.items():
+      newFile.write(str(key[0]) + ' ' + str(key[1]))
+      newFile.write(' ' + str(round(value, 3)) + '\n')
+   newFile.close()
+
+def createDict(buildFile, newFile):
+   fixedCells = 0
+   cellNumber = int(buildFile[0])
+   lowerLimit = float(buildFile[1])
+   upperLimit = float(buildFile[2])
+   cellRoot = int(math.sqrt(cellNumber))
+   d = {}
+   for x in range(0, cellRoot):
+      for y in range(0, cellRoot):
+         if y < 1 or y >= cellRoot - 1:
+            fixedCells += 1
+            d[(x, y)] = random.uniform(lowerLimit, upperLimit)
+         else:
+            if x == 0 or x == cellRoot - 1:
+               fixedCells += 1
+               d[(x, y)] = random.uniform(lowerLimit, upperLimit)
+   
+   newFile.write(str(fixedCells) + '\n')
+   return d
+
+if __name__== "__main__":
+   buildFile = getCellNumber()
+   newFile = openFile(buildFile[0])
+   cellDict = createDict(buildFile, newFile)
+   writeToFile(newFile, buildFile, cellDict)
